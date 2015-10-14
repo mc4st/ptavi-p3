6eeb4159e9e3e71c19e1dc369cc02c581796a90f
mc4st <C4st3j0n>
2015-10-14 21:57:06 +0200

    Optimizado

---
+++ smallsmilhandler.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+class SmallSmilHandler(ContentHandler):
+    " Clase para manejar fichero smil"
+    def __init__ (self):
+
+        self.tags = []
+        self.diccetiquetas = {"root-layout": ['width', 'height', 'back-groundcolor'],
+                              "region": ['id', 'top', 'bottom', 'left','right'],
+                              "img": ['src', 'region', 'begin', 'dur'],
+                              "audio": ['src', 'begin', 'dur'],
+                              "textstream": ['src', 'region']}
+
+    def startElement(self, etiqueta, attrs):
+        if etiqueta in self.diccetiquetas:
+            tmpdicc = {}
+            for atributo in self.diccetiquetas[etiqueta]:
+                tmpdicc[atributo] = attrs.get(atributo, "")
+            self.tags.append([etiqueta, tmpdicc])
+
+    def get_tags(self):
+        return self.tags
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    cHandler = SmallSmilHandler()
+    parser.setContentHandler(cHandler)
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
