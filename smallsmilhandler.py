6de35c76c3a5e4434616b2a2d09b2d73cf0a33b8
mc4st <C4st3j0n>
2015-10-17 13:14:09 +0200

    Arreglado nombres

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,13 +4,16 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-class SmallSmilHandler(ContentHandler):
+
+class SmallSMILHandler(ContentHandler):
+
     " Clase para manejar fichero smil"
-    def __init__ (self):
+    def __init__(self):
 
         self.tags = []
-        self.diccetiquetas = {"root-layout": ['width', 'height', 'back-groundcolor'],
-                              "region": ['id', 'top', 'bottom', 'left','right'],
+        self.diccetiquetas = {"root-layout": ['width', 'height',
+                              'back-groundcolor'],
+                              "region": ['id', 'top', 'bottom', 'left', 'right'],
                               "img": ['src', 'region', 'begin', 'dur'],
                               "audio": ['src', 'begin', 'dur'],
                               "textstream": ['src', 'region']}
@@ -30,7 +33,7 @@ if __name__ == "__main__":
     Programa principal
     """
     parser = make_parser()
-    cHandler = SmallSmilHandler()
+    cHandler = SmallSMILHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open('karaoke.smil'))
     print(cHandler.get_tags())
