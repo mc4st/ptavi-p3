d28713fd10e1fa01d99ea7d0029c096dadb7ea14
mc4st <C4st3j0n>
2015-10-18 12:21:08 +0200

    Ap 4

--- karaoke.py
+++ karaoke.py
@@ -15,14 +15,26 @@ class KaraokeLocal():
         try:
             parser.parse(open(fichero))
             self.lista = cHandler.get_tags()
-            print(self.lista)
         except:
             sys.exit("Error del fichero")
+    def __str__(self):
+        cadena_total = ""
+        for atributos in self.lista:
+            dic = atributos[1]
+            cadena = ""
+            for clave in dic.keys():
+                if dic[clave] != "":
+                    cadena = cadena + "\t" + clave + '="' + dic[clave] + '"'
+            cadena_total = cadena_total + atributos[0] + cadena
+            if atributos != self.lista[-1]:
+                cadena_total = cadena_total + "\n"
+        return cadena_total
 
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
         fichero = sys.argv[1]
         karaoke = KaraokeLocal(fichero)
+        print(karaoke)
     else:
         sys.exit("Usage: python3 karaoke.py file.smil")
