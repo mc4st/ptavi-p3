c5b03b3a1e4e014936344908f9ee434dc98ae7f1
mc4st <C4st3j0n>
2015-10-19 11:27:53 +0200

    Terminado do_local

--- karaoke.py
+++ karaoke.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import urllib.request
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
@@ -9,6 +10,7 @@ from smallsmilhandler import SmallSMILHandler
 
 class KaraokeLocal():
     def __init__(self, fichero):
+        "Inicializador"
         parser = make_parser()
         cHandler = SmallSMILHandler()
         parser.setContentHandler(cHandler)
@@ -17,7 +19,9 @@ class KaraokeLocal():
             self.lista = cHandler.get_tags()
         except:
             sys.exit("Error del fichero")
+
     def __str__(self):
+        "Cadenas"
         cadena_total = ""
         for atributos in self.lista:
             dic = atributos[1]
@@ -30,11 +34,22 @@ class KaraokeLocal():
                 cadena_total = cadena_total + "\n"
         return cadena_total
 
+    def do_local(self):
+        "Descarga de ficheros"
+        for atributos in self.lista:
+            dic = atributos[1]
+            for clave in dic.keys():
+                if dic[clave].split(':')[0] == "http":
+                    urllib.request.urlretrieve(dic[clave], dic[clave].split('/')[-1])
+                    dic[clave] = dic[clave].split('/')[-1]
 if __name__ == "__main__":
 
     if len(sys.argv) == 2:
         fichero = sys.argv[1]
         karaoke = KaraokeLocal(fichero)
-        print(karaoke)
+        print(karaoke.__str__())
+        karaoke.do_local()
+        print("")
+        print(karaoke.__str__())
     else:
         sys.exit("Usage: python3 karaoke.py file.smil")
